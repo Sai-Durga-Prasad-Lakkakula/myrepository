bef397b81ef74b0f768bffbcdb24a52e9189a0c6
lapr1221 <lakkakula.sai.prasad@netcracker.com>
2023-01-02 21:24:34 +0530

    Create CountingSort.java
    Frequency array

---
+++ MyPracticeCodes/src/com/algorithm/CountingSort.java
@@ -0,0 +1,21 @@
+//Frequency array
+
+package com.algorithm;
+
+import java.util.Scanner;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int a[] = new int[100];
+
+        for (int i = 0; i < N; i++) {
+            a[sc.nextInt()]++;
+        }
+        for (int j = 0; j < N; j++) {
+            System.out.print(a[j] + " ");
+        }
+        sc.close();
+    }
+}
